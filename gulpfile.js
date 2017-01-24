0ba5bb3385c8c83887f00e2dd08d4d436868da7e
chinatown <ivan.tokarenko@gmail.com>
2017-01-24 07:14:38 +0200

    ебись оно конем

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 var imageop = require('gulp-image-optimization');
+const image = require('gulp-image');
 
 // Default Task
 gulp.task('default', ['watch']);
@@ -10,9 +11,9 @@ gulp.task('watch', function() {
     gulp.watch('../porno_images/', ['images']);
 });
 
-// Отличный компресс
+// Отличный компресс но работает только на маке почему то
 gulp.task('images', function(cb) {
-    gulp.src(['../porno_images/**/*']).pipe(imageop({
+    gulp.src(['../porno_images_old/**/*']).pipe(imageop({
         optimizationLevel: 5,
         progressive: true,
         interlaced: true
@@ -52,5 +53,24 @@ gulp.task('compressbig', function() {
         .pipe(gulp.dest('../porno_images/'));
 });
 
+
+//  Просто не работает
+gulp.task('image', function () {
+    gulp.src('../porno_images_old/**/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            jpegoptim: true,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10
+        }))
+        .pipe(gulp.dest('../porno_images/'));
+});
+
+gulp.task('default', ['image']);
 
 
