d652092fb2241e76ee6d6bdd47bb1113cb71f057
chinatown <ivan.tokarenko@gmail.com>
2017-01-24 06:41:46 +0200

    Отличное сжатие

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
+var imageop = require('gulp-image-optimization');
 
 // Default Task
 gulp.task('default', ['watch']);
 
 // Watch Task
 gulp.task('watch', function() {
-    gulp.watch('./*', ['compress']);
+    gulp.watch('../porno_images/', ['images']);
 });
 
+// Отличный компресс
+gulp.task('images', function(cb) {
+    gulp.src(['../porno_images/**/*', '../porno_images/*','../porno_images/***/**/*']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('../porno_images/')).on('end', cb).on('error', cb);
+});
+
+
+// Хуевый компресс вообще
 // Compress Task
 gulp.task('compress', function() {
     gulp.src('../porno_images/***/**/*')
@@ -40,13 +52,5 @@ gulp.task('compressbig', function() {
         .pipe(gulp.dest('../porno_images/'));
 });
 
-gulp.task('test', function() {
-
-    gulp.src('../porno_images/195/big-domashnee-porno-parnyu-prishlos-smotret-na-izmenu-russkoy-telki.jpg')
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest('../porno_images/195/*'));
-});
 
 
